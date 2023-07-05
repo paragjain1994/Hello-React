612e3dfc3aacc56e14d8bbac2544a761105e3183
parag <paragjain528@gmail.com>
2023-07-05 23:52:57 +0530

    working with useEffect hook

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,9 +1,25 @@
 import RestaurantCard from "./RestaurantCard";
-import resList from "../utils/mockData";
-import { useState } from "react";
+// import resList from "../utils/mockData";
+import { useState, useEffect } from "react";
 
 const Body = () => {
-  const [listOfRestaurants, setListOfRestraunt] = useState(resList);
+  const [listOfRestaurants, setListOfRestraunt] = useState([]);
+
+  useEffect(() => {
+    console.log("useEffect called");
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&page_type=DESKTOP_WEB_LISTING"
+    );
+
+    const json = await data.json();
+    console.log(json);
+    // Optional Chaining
+    setListOfRestraunt(json?.data?.cards[2]?.data?.data?.cards);
+  }
 
   return (
     <div className="body">

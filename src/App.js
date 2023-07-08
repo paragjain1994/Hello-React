4e6fae5cce576ae144c3430bfc176ceb961369b3
parag <paragjain528@gmail.com>
2023-07-08 20:04:44 +0530

    Showing Error page

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import Header from "./components/Header";
 import Body from "./components/Body";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import About from "./components/About";
-
+import Contact from "./components/Contact";
+import Error from "./components/Error";
 
 const AppLayout = () => {
   return (
@@ -16,10 +17,10 @@ const AppLayout = () => {
 };
 
 const appRouter = createBrowserRouter([
-  { path: "/", element: <AppLayout /> },
+  { path: "/", element: <AppLayout />, errorElement: <Error/> },
   { path: "/about", element: <About /> },
+  { path: "/contact", element: <Contact /> }
 ]);
-
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 

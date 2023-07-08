2d8ab6570d341ff3f63d8d8171b28ce9d43e7ec3
parag <paragjain528@gmail.com>
2023-07-08 13:30:35 +0530

    Routing configuration with first page About

--- src/App.js
+++ src/App.js
@@ -2,13 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
-
-
-
-
-
-
-
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import About from "./components/About";
 
 
 const AppLayout = () => {
@@ -20,6 +15,12 @@ const AppLayout = () => {
   );
 };
 
+const appRouter = createBrowserRouter([
+  { path: "/", element: <AppLayout /> },
+  { path: "/about", element: <About /> },
+]);
+
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<AppLayout />);
+root.render(<RouterProvider router={appRouter} />);
